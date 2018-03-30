e9d161b21f2f8f9704a861c335cdee66c81c78cc
apaar <apaar.gupta.13@gmail.com>
2018-03-30 15:53:01 +0530

    mongo still does not work?

--- server.go
+++ server.go
@@ -1,77 +1,74 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 
+	"github.com/Shopify/sarama"
 	"github.com/gorilla/mux"
-	"github.com/segmentio/kafka-go"
 )
 
-var stopInputDaemon chan struct{}
-var stopOutputDaemon chan struct{}
-var kafkaInputChannel chan []byte
-var kafkaOutputChannel chan []byte
+const topic = "wrs"
+
+var brokers = []string{"127.0.0.1:9092"}
 
 type Message struct {
 	Id   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
-func commitToKafka() {
-	topic := "wrs"
-	partition := 0
+func newProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer(brokers, config)
 
-	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	for {
-		select {
-		case <-stopInputDaemon:
-			break
-		case message := <-kafkaInputChannel:
-			conn.WriteMessages(
-				kafka.Message{Value: message},
-			)
-		}
-	}
-	conn.Close()
+	return producer, err
 }
 
-func readFromKafka() {
-
-	for {
-		topic := "wrs"
-		partition := 0
-		conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
-
-		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-		batch := conn.ReadBatch(1, 1e6)
-
-		b := make([]byte, 10e3) // 10KB max per message
-		for {
-			_, err := batch.Read(b)
-			if err != nil {
-				break
-			}
-			fmt.Println(string(b))
-		}
-		batch.Close()
-		conn.Close()
-		select {
-		case <-stopOutputDaemon:
-			break
-		}
+func prepareMessage(topic, message string) *sarama.ProducerMessage {
+	msg := &sarama.ProducerMessage{
+		Topic:     topic,
+		Partition: -1,
+		Value:     sarama.StringEncoder(message),
 	}
 
+	return msg
+}
+
+func subscribe(topic string, consumer sarama.Consumer) {
+	partitionList, err := consumer.Partitions(topic) //get all partitions on the given topic
+	if err != nil {
+		fmt.Println("Error retrieving partitionList ", err)
+	}
+	initialOffset := sarama.OffsetOldest //get offset for the oldest message on the topic
+
+	for _, partition := range partitionList {
+		pc, _ := consumer.ConsumePartition(topic, partition, initialOffset)
+
+		go func(pc sarama.PartitionConsumer) {
+			for message := range pc.Messages() {
+				messageReceived(message)
+			}
+		}(pc)
+	}
+}
+
+func messageReceived(message *sarama.ConsumerMessage) {
+	fmt.Println(string(message.Value))
 }
 
 // curl localhost:8000 -d '{"name":"Hello"}'
 func Producer(w http.ResponseWriter, r *http.Request) {
+	producer, err := newProducer()
+	if err != nil {
+		fmt.Println("Could not create producer: ", err)
+	}
+
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -92,25 +89,30 @@ func Producer(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	kafkaInputChannel <- output
-	w.WriteHeader(http.StatusOK)
+	msg1 := prepareMessage(topic, string(output))
+	partition, offset, err := producer.SendMessage(msg1)
+	if err != nil {
+		fmt.Fprintf(w, "%s error occured.", err.Error())
+	} else {
+		fmt.Fprintf(w, "Message was saved to partion: %d.\nMessage offset is: %d.\n", partition, offset)
+	}
 }
 
 func main() {
-	stopInputDaemon = make(chan struct{})
-	stopOutputDaemon = make(chan struct{})
-	kafkaInputChannel = make(chan []byte, 1024)
-	kafkaOutputChannel = make(chan []byte, 1024)
-	go commitToKafka()
-	go readFromKafka()
+
+	consumer, err := sarama.NewConsumer(brokers, nil)
+	if err != nil {
+		fmt.Println("Could not create consumer: ", err)
+	}
+
+	subscribe(topic, consumer)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", Producer).Methods("POST")
 	address := ":8000"
 	log.Println("Starting server on address", address)
-	err := http.ListenAndServe(address, r)
+	err = http.ListenAndServe(address, r)
 	if err != nil {
-		close(stopInputDaemon)
-		close(stopOutputDaemon)
 		panic(err)
 	}
 }
